66ba03fa38130a030a45dc440dd99ad74be3eb08
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-27 13:24:39 +0800

    Initial Commit

---
+++ ormconfig.ts
@@ -0,0 +1,28 @@
+import { SqlServerConnectionOptions } from 'typeorm/driver/sqlserver/SqlServerConnectionOptions'
+import { config as dotEnvConfig } from 'dotenv'
+
+dotEnvConfig()
+const config: SqlServerConnectionOptions & {
+  options: {
+    trustServerCertificate: boolean
+  }
+} = {
+  type: 'mssql',
+  host: '192.168.1.14',
+  port: 1433,
+  username: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DB,
+
+  entities: ['dist/src/**/*.entity.js'],
+  migrations: ['dist/src/db/migrations/*.js'],
+  options: {
+    trustServerCertificate: true,
+  },
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+  synchronize: false,
+}
+
+export default config
