81299c5e911af337eb3172fc9f00f0c0de10fbf5
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-29 13:25:34 +0800

    Initial Commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Bcrypt } from 'src/utils/Bcrypt'
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @Column()
+  name: string
+
+  @Column({
+    unique: true,
+  })
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  updated_at: Date
+
+  @BeforeInsert()
+  updateUpdatedAt() {
+    this.updated_at = new Date()
+  }
+}
