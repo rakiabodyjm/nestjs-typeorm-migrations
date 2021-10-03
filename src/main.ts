5531671619747928c59581f25a3b1cd00b9657c2
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-04 06:51:06 +0800

    Exclude password in respose and enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await await NestFactory.create(AppModule)
+  app.enableCors()
   await app.listen(5000)
 }
 bootstrap()
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer'
 import { Bcrypt } from 'src/utils/Bcrypt'
 import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
@@ -14,6 +15,7 @@ export class User {
   })
   email: string
 
+  @Exclude()
   @Column()
   password: string
 
