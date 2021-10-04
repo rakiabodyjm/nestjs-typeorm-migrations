0dc70a7ca834a2913c9c71c299ed2d0017ae7cdb
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-04 20:37:25 +0800

    Added books entity

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await await NestFactory.create(AppModule)
-  app.enableCors()
+  const app = await await (
+    await NestFactory.create(AppModule)
+  ).useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  )
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  })
+
   await app.listen(5000)
 }
 bootstrap()
